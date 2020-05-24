29e4a26bc9964f30fe154b4c54e80bfc7fef7938
estherbabu77 <57264514+estherbabu77@users.noreply.github.com>
2020-05-24 11:03:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var ball;
+var database;
+var position;
+var player;
+var form;
+var game;
+var gameState = 0;
+var playerCount;
+
+function setup(){
+    database = firebase.database();
+     createCanvas(500,500);
+     
+
+}
+
+function draw(){
+    background("white");
+    
+}
+
